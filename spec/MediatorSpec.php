c8ce454c09f78f642468a99338387e7bb8792ce3
Garrett W <garrettw87@gmail.com>
2015-05-12 17:29:49 +0000

    updated tests, removed old code

--- spec/MediatorSpec.php
+++ spec/MediatorSpec.php
@@ -5,11 +5,11 @@ namespace spec\Noair;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
-class NoairSpec extends ObjectBehavior
+class MediatorSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
-        $this->shouldBeAnInstanceOf('Noair\Noair');
+        $this->shouldBeAnInstanceOf('Noair\Mediator');
     }
 
     function it_will_not_hold_unheard_events_by_default()
---
+++ spec/ObserverSpec.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace spec\Noair;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ObserverSpec extends ObjectBehavior
+{
+    public function let()
+    {
+        $this->beAnInstanceOf('spec\Noair\ObserverExample');
+        $this->beConstructedWith(new \Noair\Mediator());
+        $this->subscribe();
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldBeAnInstanceOf('Noair\AbstractObserver');
+        $this->shouldBeAnInstanceOf('spec\Noair\ObserverExample');
+    }
+
+    function it_keeps_an_instance_of_mediator()
+    {
+        $this->mediator->shouldBeAnInstanceOf('Noair\Mediator');
+    }
+
+    function it_responds_to_all_events()
+    {
+        $this->mediator->publish(new \Noair\Event('random'))->shouldReturn('random');
+    }
+}
+
+class ObserverExample extends \Noair\AbstractObserver
+{
+    public function subscribe() {
+        // This is just here for an example of explicitly-defined handlers
+        $this->handlers = [
+            'all' => [[$this, 'log'], \Noair\Mediator::PRIORITY_URGENT, true],
+            'one' => [[$this, 'handlerOne']],
+        ];
+        parent::subscribe();
+    }
+
+    public function handlerOne(\Noair\Event $e)
+    {
+        return 'one';
+    }
+
+    public function onTwo(\Noair\Event $e)
+    {
+        return 'two';
+    }
+
+    public function log(\Noair\Event $e)
+    {
+        return $e->name;
+    }
+}
